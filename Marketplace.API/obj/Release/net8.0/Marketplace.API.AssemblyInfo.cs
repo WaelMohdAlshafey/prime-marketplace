6af6c29abb48f37f59cf779f730b00e6e8e34d89
WaelMohdAlshafey <smael7108@gmail.com>
2026-08-23 11:21:45 +0300

    fix: configure CartItem.Id as database-generated identity (Fluent API + attribute)

--- Marketplace.API/obj/Release/net8.0/Marketplace.API.AssemblyInfo.cs
+++ Marketplace.API/obj/Release/net8.0/Marketplace.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Marketplace.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+314887a6e28a19146288990e2f63ba68afd490ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+709b374a8825b4eafbcf8679b66fa220e9274d86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Marketplace.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Marketplace.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Marketplace.Domain/Entities/CartItem.cs
+++ Marketplace.Domain/Entities/CartItem.cs
@@ -1,13 +1,16 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Marketplace.Domain.Entities;
 
 public class CartItem
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-    public int UserId { get; set; }        // The buyer (VendorId from token)
+
+    public int UserId { get; set; }
     public int ProductId { get; set; }
     public int Quantity { get; set; }
     public DateTime AddedAt { get; set; } = DateTime.UtcNow;
 
-    // Navigation property
     public virtual Product? Product { get; set; }
 }
